0b1dfd39bbb89d5f2ba925394b79c29d546c9634
tylerreed <tylerreedytlearning@gmail.com>
2024-04-29 22:15:55 -0400

    updated logging for pandas

--- 06-logging/main.py
+++ 06-logging/main.py
@@ -52,4 +52,33 @@ def str_to_dict(s):
 
 
 # use pandas to get extra information and print out to terminal
+def get_log(dbname="logs.db", table="chat_completions"):
+    import sqlite3
 
+    con = sqlite3.connect(dbname)
+    query = f"SELECT request, response, cost, start_time, end_time from {table}"
+    cursor = con.execute(query)
+    rows = cursor.fetchall()
+    column_names = [description[0] for description in cursor.description]
+    data = [dict(zip(column_names, row)) for row in rows]
+    con.close()
+    return data
+
+def str_to_dict(s):
+    return json.loads(s)
+
+
+log_data = get_log()
+log_data_df = pd.DataFrame(log_data)
+
+log_data_df["total_tokens"] = log_data_df.apply(
+    lambda row: str_to_dict(row["response"])["usage"]["total_tokens"], axis=1
+)
+
+log_data_df["request"] = log_data_df.apply(lambda row: str_to_dict(row["request"])["messages"][0]["content"], axis=1)
+
+log_data_df["response"] = log_data_df.apply(
+    lambda row: str_to_dict(row["response"])["choices"][0]["message"]["content"], axis=1
+)
+
+print(log_data_df)
